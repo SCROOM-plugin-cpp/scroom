8352e79f829096fbdcfc9f5e9c1ae8bc54fd2784
Rick de Jager <rickdejager99@gmail.com>
2020-05-02 11:18:36 +0200

    Added unit tests to init script

--- build_scripts/init.sh
+++ build_scripts/init.sh
@@ -1,45 +1,29 @@
 # First get the scroom repository
-if [ "1" == "1" ]; then
-  # Fetch all required packages on clone too
-  apt update
-  apt upgrade
-  apt install -y git gawk libboost-test-dev libboost-dev libboost-filesystem-dev libboost-program-options-dev libboost-system-dev libboost-thread-dev libgtest-dev google-mock libcairo2-dev libglade2-dev libglib2.0-dev libgtk2.0-dev libtiff5-dev pkg-config doxygen graphviz ccache libc6-dev
 
-  # git clone --depth 200 https://github.com/kees-jan/scroom.git
-  cd scroom
-  autoreconf -i
-  cd ..
-else
-  echo "Skipping scroom setup..."
-fi
+# Fetch all required packages on clone too
+apt update
+apt upgrade
+apt install -y git gawk libboost-test-dev libboost-dev libboost-filesystem-dev libboost-program-options-dev libboost-system-dev libboost-thread-dev libgtest-dev google-mock libcairo2-dev libglade2-dev libglib2.0-dev libgtk2.0-dev libtiff5-dev pkg-config doxygen graphviz ccache libc6-dev
 
-# Then maybe configure it
+cd scroom
+autoreconf -i
+cd ..
+
+# Then configure it
 path=$(pwd)/scroom-install
-if [ "1" == "1" ]; then
-  mkdir scroom-install
-  cd scroom
-  mkdir scroom-build
-  cd scroom-build
-  ../configure CC="ccache gcc" CXX="ccache g++" --prefix $path
-  cd ../..
-  build=1
-else
-  echo "Skipping scroom build configuration..."
-  if [ "$1" == "true" ]; then
-    build=1
-  else
-    build=0
-  fi
-fi
 
-# Then maybe build it
-if [ $build -eq 1 ]; then
-  cd ./scroom/scroom-build
-  make -k -j1 install
-  cd ../..
-else
-  echo "Skipping scroom build..."
-fi
+mkdir scroom-install
+cd scroom
+mkdir scroom-build
+cd scroom-build
+../configure CC="ccache gcc" CXX="ccache g++" --prefix $path
+cd ../..
+
+# Next, test and build scroom
+cd ./scroom/scroom-build
+make -k -j1 check
+make -k -j1 install
+cd ../..
 
 # Then maybe add a run shortcut
 if [ ! -f "run-scroom.sh" ]; then
@@ -48,42 +32,3 @@ if [ ! -f "run-scroom.sh" ]; then
 else
   echo "Skipping run script..."
 fi
-
-# Then get our plugin
-if [ "1" == "2" ]; then
-  # HTTPS
-  git clone https://github.com/SCROOM-plugin-cpp/SCROOM-plugin-cpp.git
-  # SSH:
-  # git clone git@github.com:SCROOM-plugin-cpp/SCROOM-plugin-cpp.git
-  cd SCROOM-plugin-cpp
-  autoreconf -i
-  cd ..
-else
-  echo "Skipping plugin setup..."
-fi
-
-# Then maybe configure our plugin
-if [ "1" == "2" ]; then
-  cd ./SCROOM-plugin-cpp
-  mkdir plugin-build
-  cd plugin-build
-  ../configure CC="ccache gcc" CXX="ccache g++" CPPFLAGS=-I$path/include/ LDFLAGS="-L$path/lib -Wl,-z,defs"
-  cd ../..
-else
-  echo "Skipping plugin build configuration..."
-fi
-
-# Then maybe make a plugin build script
-if [ "1" == "2" ]; then
-  echo "cd ./SCROOM-plugin-cpp/plugin-build" >> build-plugin.sh
-  echo "make -k -j" >> build-plugin.sh
-  echo "cd ../.." >> build-plugin.sh
-  echo "rm -f ./scroom-install/lib/scroom/libspexample.*" >> build-plugin.sh
-  echo "cp ./SCROOM-plugin-cpp/plugin-build/plugins/example/src/.libs/* ./scroom-install/lib/scroom/" >> build-plugin.sh
-  echo "echo \"Done! Run scroom using the generated 'run-scroom.sh' script.\"" >> build-plugin.sh
-  chmod 755 build-plugin.sh
-else
-  echo "Skipping plugin build script..."
-fi
-
-# echo "Build the plugin by running the generated 'build-plugin.sh' script."
